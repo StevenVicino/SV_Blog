050b5ac8acdc3665066e3ce21ae010f6b9ae5e79
Steven Vicino <stevenvicino@gmail.com>
2023-02-22 17:21:41 -0500

    added posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,9 @@
 import Head from 'next/head';
 import {PostCard, PostWidget, Categories} from "../components/"
+import {getPosts} from "../services"
 
-const posts = [
-  {title: "React Testing", excerpt: "Learn React Testing"},
-  {title: "React with Tailwind", excerpt: "Learn React with Tailwind"}
-]
 
-export default function Home(){
+export default function Home({posts}){
   return (
     <div className="container mx-auto px-10 mb-8 ">
       <Head>
@@ -15,7 +12,7 @@ export default function Home(){
       </Head>
 <div className="grid grid-col-1 lg:grid-cols-12 gap-12">
   <div className="lg:col-span-8 col-span-1">
-  {posts.map((post, index) =>(    <PostCard post={post} key={post.title} />))}
+  {posts.map((post, index) =>(    <PostCard post={post.node} key={post.title} />))}
   </div>
   <div className="lg:col-san-4 col-span-1">
 <div className="lg:sticky relative top-8">
@@ -29,3 +26,8 @@ export default function Home(){
   )
 }
 
+export async function getStaticProps(){
+  const posts = (await getPosts()) || [];
+
+  return {props:{posts}}
+}
